55d59ff05d892449b146adeac5754e557121e431
Richard Elijah <66692255+Oyerindeelikay@users.noreply.github.com>
2026-01-20 16:40:03 +0100

    Update settings.py

--- livkit_backend/backend/backend/settings.py
+++ livkit_backend/backend/backend/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     'django_celery_beat',
 
     'streaming',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
