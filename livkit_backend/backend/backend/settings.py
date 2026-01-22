a028b99ca06c2fe8a582212f0007375ebb9258a4
Richard Elijah <66692255+Oyerindeelikay@users.noreply.github.com>
2026-01-22 08:52:05 +0100

    Update settings.py

--- livkit_backend/backend/backend/settings.py
+++ livkit_backend/backend/backend/settings.py
@@ -222,10 +222,14 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [{
+                "address": os.environ["REDIS_URL"],
+                "ssl_cert_reqs": None,
+            }],
         },
     },
 }
+
 
 
 
